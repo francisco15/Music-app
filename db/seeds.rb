0e4c7da9e4e6869c519edb3203db3da716caef8b
francisco15 <franciscohuertas38@gmail.com>
2017-02-05 16:12:01 -0500

    agregada vista

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Artist.create(name:"Maroon 5", image_url:"http://i.ytimg.com/vi/nn5aL1RMYeM/maxresdefault.jpg")
+Artist.create(name:"Bruno Mars", image_url:"http://www.lax965.com/wp-content/uploads/2015/03/Bruno-Mars-Treauser.jpg")
+Artist.create(name:"Kayne West", image_url:"https://www.lapatilla.com/site/wp-content/uploads/2015/08-25/kanye.jpg")
+Artist.create(name:"Kase O", image_url:"http://www.go-mag.com/imagenes/kaseo_no__7038.jpg")
+
+Album.create(name:"Songs About Jane", image_url:"https://upload.wikimedia.org/wikipedia/en/b/be/Maroon_5_-_Songs_About_Jane.png", released_at: 2002, artist_id:1)
+Album.create(name:"It Won't Be Soon Before Long", image_url:"http://ecx.images-amazon.com/images/I/51PQoB8+6eL._SY300_.jpg", released_at: 2007, artist_id:1)
+Album.create(name:"Hands All Over", image_url:"https://upload.wikimedia.org/wikipedia/en/7/71/Maroon_5_-_Hands_All_Over.png", released_at: 2010, artist_id:1)
+Album.create(name:"Overexposed", image_url:"http://k39.kn3.net/taringa/2/2/9/4/2/6/7/lab09/BEE.jpg?8203", released_at: 2012, artist_id:1)
+Album.create(name:"V", image_url:"https://upload.wikimedia.org/wikipedia/en/5/53/Maroon_5_-_V_(Official_Album_Cover).png", released_at: 2014, artist_id:1)
+
+Album.create(name:"Unorthodox Jukebox", image_url:"http://www.unorthodoxjukebox.com/images/fb-img.jpg", released_at: 2012, artist_id:2)
+Album.create(name:"The Lost Planet", image_url:"http://4.bp.blogspot.com/-z2pJX3vj2qg/TeZraz_kSNI/AAAAAAAAAYE/hPmjOTMAByM/s1600/00_DJ_Xfactor_and_Bruno_Mars_Presents_the_Lost_Planet-2010-Front-PromoTorrents_com_.jpg", released_at: 2010, artist_id:2)
+Album.create(name:"Doo-Wops & Hooligans", image_url:"http://www.brunomars.com/sites/g/files/g2000001576/f/styles/album_detail_mobile_image/public/cbf3817a5e0c5f3ae6f665627f85df33.jpg?itok=fDkbhiDr", released_at: 2010, artist_id:2)
+Album.create(name:"It's Better If You Don't Understand", image_url:"http://images.coveralia.com/audio/b/Bruno_Mars-It_s_Better_If_You_Don_t_Understand_EP-Frontal.jpg", released_at: 2010, artist_id:2)
